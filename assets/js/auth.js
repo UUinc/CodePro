fa0dda4c62c351e49c12c3df9c0c0d864aa05ce3
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-12-20 22:54:07 +0100

    add basic login condition

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,7 +1,47 @@
+//Sign up data
+const signup_username = document.getElementById("signupInputUsername");
+const signup_email = document.getElementById("signupInputEmail");
+const signup_password = document.getElementById("signupInputPassword");
+const signup_password2 = document.getElementById("signupInputPassword2");
+//Log in data
+const login_email = document.getElementById("loginInputEmail");
+const login_password = document.getElementById("loginInputPassword");
+
 function Signup($location) {
-    document.location = $location;
+    const user = FindUser(signup_email.value);
+
+    if (user !== null) {
+        //show error message...
+        //email already exist
+        //empty fields
+        return;
+    }
+
+    if (1 /*if all fields are not empty and correct*/) {
+        //..add this data to db
+        document.location = $location;
+    }
 }
 
 function Login($location) {
-    document.location = $location;
+    const user = FindUser(login_email.value);
+    console.log(user);
+    if (user === null) {
+        alert("email does not exist please sign up");
+        //show error message...
+        return;
+    }
+
+    if (user.password === login_password.value) {
+        document.location = $location;
+        return;
+    }
+    alert("check your password!");
+}
+
+function FindUser(email) {
+    for (let index in users) {
+        if (users[index].email === email) return users[index];
+    }
+    return null;
 }
---
+++ assets/template/js/usersData.js
@@ -0,0 +1,12 @@
+var users = [
+    {
+        username: "yahya.lz",
+        email: "duo.lz.yahya@gmail.com",
+        password: "12345678",
+    },
+    {
+        username: "admin",
+        email: "admin@admin.com",
+        password: "admin123",
+    },
+];
